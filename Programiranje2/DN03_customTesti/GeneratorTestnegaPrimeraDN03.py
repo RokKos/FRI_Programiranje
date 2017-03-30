2bd4cb958c0ae034450e18b5e87da4ecbb4e6875
Rok Kos <kosrok97@gmail.com>
2017-03-30 23:43:23 +0200

    Updated test cases(there was two items on same spot) Thanks to Deni Cerovac for spotting mistake.

--- Programiranje2/DN03_customTesti/GeneratorTestnegaPrimeraDN03.py
+++ Programiranje2/DN03_customTesti/GeneratorTestnegaPrimeraDN03.py
@@ -1,15 +1,19 @@
 from random import randint as ri
 MAX = 1000
+porabljene_kordinate = set()
 predmeti = ri(0,MAX)
 koraki = ri(0,MAX)
 print(predmeti)
 for i in range(predmeti):
 	x = ri(-100,100)
 	y = ri(-100,100)
-	while (x == 0 and y == 0):
+	while (x == 0 and y == 0 and (x,y) in porabljene_kordinate):
+
 		x = ri(0,200)
 		y = ri(0,200)
+	porabljene_kordinate.add((x,y))
 
 	print ("{0} {1} {2}".format(x, y, ri(1,3)))
-
+print(porabljene_kordinate)
 print (koraki)
+
