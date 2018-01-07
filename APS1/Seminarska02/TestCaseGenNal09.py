083763fe7a460217b4888c0c5e0d8c297cad8bec
Rok Kos <rocki5555@gmail.com>
2018-01-07 21:54:33 +0100

    Semirska02-Naloga09: Modification testcase generator

--- APS1/Seminarska02/TestCaseGenNal09.py
+++ APS1/Seminarska02/TestCaseGenNal09.py
@@ -11,11 +11,12 @@ def pick_n_and_m():
 	m = randint(0, int(n**2))
 	return (n, m)
 
-def pick_connection(used_connections):
+def pick_connection(used_connections, cicles):
 	while True:
 		a,b = randint(1,n), randint(1,n)
 		tup = (a,b)
-		is_valid = a != b and tup not in used_connections
+		tup_inv = (tup[1], tup[0])
+		is_valid = a != b and tup not in used_connections and (cicles or tup_inv not in used_connections)
 		if is_valid:
 			return tup
 
@@ -23,9 +24,9 @@ def pick_connection(used_connections):
 if __name__ == "__main__":
 	n, m = pick_n_and_m()
 	print(n)
-	print(m)
+	print(n)
 	used_connections = set()
-	for connection in range(m):
-		tup = pick_connection(used_connections)
+	for connection in range(n):
+		tup = pick_connection(used_connections, False)
 		used_connections.add(tup)
 		print(str(tup[0]) + "," + str(tup[1]))
