7e20a65d415824ce78311aabcf64e58a973f803b
Rok Kos <rocki5555@gmail.com>
2019-01-07 20:04:25 +0100

    Numericne metode: Koncana 1 naloga za kviz 2

--- NumericneMetode/PripravaNaKviz2/Naloga1.m
+++ NumericneMetode/PripravaNaKviz2/Naloga1.m
@@ -24,6 +24,34 @@ function Naloga1
     end
   end
   
+  function x = GaussSeidel(A, b, x_0, epsilon, max_itter)
+    n = length(b);
+    x = zeros(n,1);
+    for itt = 2:max_itter
+          for i = 1:n
+            sum_r_pp = 0;
+            for j = 1:(i-1)
+              sum_r_pp += A(i,j) * x(j);              
+            end
+            
+            sum_r = 0;
+            for j = (i+1):n
+              sum_r += A(i,j) * x_0(j);              
+            end
+            
+            x(i) = 1 / A(i,i) * (b(i) - sum_r_pp - sum_r);
+          end
+          if (abs(norm(x) - norm(x_0)) < epsilon )
+              disp('Nasli smo dovolj dober priblizek')
+              return
+          end
+          
+          x_0 = x;
+      end
+    disp('Dosegli MAX_ITTER')
+    
+  end
+  
   A = [14, -6, 4, 2;
        -6, 10, -5, -2;
        4, -5, 9, 0;
@@ -32,13 +60,18 @@ function Naloga1
   b = [1,-2,0,3]'
   
   V_matlab = chol(A)'
+  % Z razcepom choleskega
   V = razcepCholeskega(A)
   
-  nazaj_A = V*V'
+  nazaj_A = V*V';
   
   y = V\b
   x = V'\y
   
+  x_seidel = GaussSeidel(A, b, [1,1,1,1]', 1e-10, 1000)
+  
+  
+  % Preverjanje
   x_matlab = A\b
   
 end
