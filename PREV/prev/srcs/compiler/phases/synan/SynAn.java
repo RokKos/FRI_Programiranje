dbb5262474413b11eb16f1a2254c74cb30d5e50a
RokKos <rocki5555@gmail.com>
2019-03-18 09:27:00 +0100

    PREV - Syntax : Implemented all productions until Type

--- PREV/prev/srcs/compiler/phases/synan/SynAn.java
+++ PREV/prev/srcs/compiler/phases/synan/SynAn.java
@@ -91,12 +91,212 @@ public class SynAn extends Phase {
 			throw new Report.Error(currSymb, errorMsg);
 	}
 
+	private void CheckAndSkip(Symbol.Term token, String errorMsg) {
+		if (currSymb.token == token) {
+			currSymb = lexAn.lexer();
+		} else {
+			throw new Report.Error(currSymb, errorMsg);
+		}
+	}
+
 	private DerNode parseSource() {
 		DerNode node = new DerNode(DerNode.Nont.Source);
-		node.add(parseDecls());
+		switch (currSymb.token) {
+			case TYP:
+			case VAR:
+			case FUN:
+				node.add(parseDecl());
+				node.add(parseDeclEps());		
+				break;
+		
+			default:
+				throw new Report.Error(currSymb.location(), "Program doesn't start with declaration. Each declaration starts with 'typ', 'var' or 'fun'");
+				
+		}
 		return node;
 	}
 
-	// TODO
+
+	private final String EXPECTED_SYMBOLS_STR = "Expected symbol ";
+	private final String LPARENTHESIS_ERR_STRING = EXPECTED_SYMBOLS_STR + "'(' got: ";
+	private final String RPARENTHESIS_ERR_STRING = EXPECTED_SYMBOLS_STR + "')' got: ";
+	private final String LBRACKET_ERR_STRING = EXPECTED_SYMBOLS_STR + "'[' got: ";
+	private final String RBRACKET_ERR_STRING = EXPECTED_SYMBOLS_STR + "']' got: ";
+	private final String SEMIC_ERR_STRING = EXPECTED_SYMBOLS_STR + "';' got: ";
+	private final String COLON_ERR_STRING = EXPECTED_SYMBOLS_STR + "':' got: ";
+	private final String IDENTIFIER_ERR_STRING = EXPECTED_SYMBOLS_STR + "IDENTIFIER got: ";
+
+
+	private DerNode parseDecl() {
+		DerNode node = new DerNode(DerNode.Nont.Decl);
+		switch (currSymb.token) {
+			case TYP:
+				add(node, Symbol.Term.TYP, EXPECTED_SYMBOLS_STR + "'typ' got: " + currSymb.token.toString());
+				node.add(parseArg());
+				CheckAndSkip(Symbol.Term.SEMIC, SEMIC_ERR_STRING + currSymb.token.toString());
+				break;
+			case VAR:
+				add(node, Symbol.Term.VAR, EXPECTED_SYMBOLS_STR + "'var' got: " + currSymb.token.toString());
+				node.add(parseArg());
+				CheckAndSkip(Symbol.Term.SEMIC, SEMIC_ERR_STRING + currSymb.token.toString());
+				break;
+			case FUN:
+				add(node, Symbol.Term.FUN, EXPECTED_SYMBOLS_STR + "'fun' got: " + currSymb.token.toString());
+				add(node, Symbol.Term.IDENTIFIER, IDENTIFIER_ERR_STRING + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseArgs());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.COLON, COLON_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				node.add(parseProxyExpr());
+				CheckAndSkip(Symbol.Term.SEMIC, SEMIC_ERR_STRING + currSymb.token.toString());
+				break;
+		
+			default:
+				throw new Report.Error(currSymb.location(), "Program doesn't start with declaration. Each declaration starts with 'typ', 'var' or 'fun'");
+				
+		}
+		
+		return node;
+	}
+
+	private DerNode parseDeclEps() {
+		DerNode node = new DerNode(DerNode.Nont.DeclsRest);
+		switch (currSymb.token) {
+			case TYP:
+			case VAR:
+			case FUN:
+				node.add(parseDecl());
+				node.add(parseDeclEps());		
+				break;
+			case RBRACE:
+			case EOF:
+				break;
+				
+			default:
+				throw new Report.Error(currSymb.location(), "Program doesn't start with declaration. Each declaration starts with 'typ', 'var' or 'fun'");
+				
+		}
+		return node;
+	}
+
+	private DerNode parseArgs() {
+		DerNode node = new DerNode(DerNode.Nont.Args);
+		switch (currSymb.token) {
+			case IDENTIFIER:
+				node.add(parseArg());
+				node.add(parseArgsEps());
+				break;
+			case RPARENTHESIS:
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), "Mistake in arguments of function call.");
+		}
+
+		return node;
+
+	}
+
+	private DerNode parseArgsEps() {
+		DerNode node = new DerNode(DerNode.Nont.ArgsEps);
+		switch (currSymb.token) {
+			case COMMA:
+				CheckAndSkip(Symbol.Term.COMMA, EXPECTED_SYMBOLS_STR + "',' got: " + currSymb.token.toString());
+				node.add(parseArg());
+				node.add(parseArgsEps());
+				break;
+			case RPARENTHESIS:
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), "Mistake in arguments of function call.");
+		}
+
+		return node;
+	}
+
+	private DerNode parseArg() {
+		DerNode node = new DerNode(DerNode.Nont.Arg);
+		switch (currSymb.token) {
+			case IDENTIFIER:
+				add(node, Symbol.Term.IDENTIFIER, IDENTIFIER_ERR_STRING + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.COLON, COLON_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				break;
+			case RPARENTHESIS:
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), "Mistake in arguments of function call.");
+		}
+
+		return node;
+	}
+
+	private DerNode parseType() {
+		DerNode node = new DerNode(DerNode.Nont.Type);
+		switch (currSymb.token) {
+			case IDENTIFIER:
+				add(node, Symbol.Term.IDENTIFIER, IDENTIFIER_ERR_STRING + currSymb.token.toString());
+				break;
+			case LPARENTHESIS:
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+
+			case VOID:
+				add(node, Symbol.Term.VOID, EXPECTED_SYMBOLS_STR + "VOID got: " + currSymb.token.toString());
+				break;
+
+			case BOOL:
+				add(node, Symbol.Term.BOOL, EXPECTED_SYMBOLS_STR + "BOOL got: " + currSymb.token.toString());
+				break;
+
+			case CHAR:
+				add(node, Symbol.Term.CHAR, EXPECTED_SYMBOLS_STR + "CHAR got: " + currSymb.token.toString());
+				break;
+
+			case INT:
+				add(node, Symbol.Term.INT, EXPECTED_SYMBOLS_STR + "INT got: " + currSymb.token.toString());
+				break;
+
+			case ARR:
+				add(node, Symbol.Term.ARR, EXPECTED_SYMBOLS_STR + "ARR got: " + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LBRACKET, LBRACKET_ERR_STRING + currSymb.token.toString());
+				node.add(parseExpr());
+				CheckAndSkip(Symbol.Term.RBRACKET, RBRACKET_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				break;
+
+			case REC:
+				add(node, Symbol.Term.REC, EXPECTED_SYMBOLS_STR + "REC got: " + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseArgs());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+
+			case PTR:
+				add(node, Symbol.Term.PTR, EXPECTED_SYMBOLS_STR + "PTR got: " + currSymb.token.toString());
+				node.add(parseType());
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), "Expected type.");
+		}
+
+		return node;
+	}
+
+
+	private DerNode parseProxyExpr() {
+		return new DerNode(DerNode.Nont.Expr);
+	}
+
+	private DerNode parseExpr() {
+		return new DerNode(DerNode.Nont.Expr);
+	}
+	
 
 }
