2cbe2334268e4201c82d9adbae3cc1b78e328961
RokKos <rocki5555@gmail.com>
2019-03-20 09:42:34 +0100

    PREV - Syntax : All binary expresions implemented

--- PREV/prev/srcs/compiler/phases/synan/SynAn.java
+++ PREV/prev/srcs/compiler/phases/synan/SynAn.java
@@ -5,6 +5,7 @@ package compiler.phases.synan;
 
 import compiler.common.report.*;
 import compiler.data.symbol.*;
+import compiler.data.symbol.Symbol.Term;
 import compiler.data.dertree.*;
 import compiler.phases.*;
 import compiler.phases.lexan.*;	
@@ -119,7 +120,21 @@ public class SynAn extends Phase {
 	private final String IDENTIFIER_ERR_STRING = EXPECTED_SYMBOLS_STR + "IDENTIFIER" + GOT_STR;
 	private final String PARAMETER_DECLARATION_ERR_STR = "Expected IDENTIFIER or ) when parsing parameters";
 	private final String PARAMETER_DECLARATION_REST_ERR_STR = "Expected , or ) when parsing parameters";
-
+	private final String TYPE_ERR_STR = "Parsing type and expected IDENTIFIER, VOID, CHAR, ARR, INT, BOOL";
+	private final String BODY_ERR_STR = "Expected ; or EXPR when parsing function declaration";
+	private final String RECORD_DECL_ERR_STR = "Expected IDENTIFIER when parsing declarations of record";
+	private final String EXPR_ERR_STR = "Expected operator or left brace while parsing expresion.";
+	private final String EXPR_DISJ_ERR_STR = "Expected operator while parsing disjunct expresion.";
+	private final String EXPR_DISJ_REST_ERR_STR = "Expected | or ^ or end of expresion while parsing disjunct expresion.";
+	private final String EXPR_CONJ_ERR_STR = "Expected operator while parsing conjuct expresion.";
+	private final String EXPR_CONJ_REST_ERR_STR = "Expected & or | or ^ or end of expresion while parsing conjuct expresion.";
+	private final String EXPR_REL_ERR_STR = "Expected operator while parsing relation expresion.";
+	private final String EXPR_REL_REST_ERR_STR = "Expected ==, !=, <=, >=, < or > while parsing relation expresion.";
+	private final String EXPR_ADD_ERR_STR = "Expected operator while parsing additive expresion.";
+	private final String EXPR_ADD_REST_ERR_STR = "Expected + or - while parsing additive expresion.";
+	private final String EXPR_MUL_ERR_STR = "Expected operator while parsing multiplicative expresion.";
+	private final String EXPR_MUL_REST_ERR_STR = "Expected *, / or % while parsing multiplicative expresion.";
+	private final String EXPR_PREF_ERR_STR = "Expected unary operator while parsing prefix expresion.";
 
 
 	// --- Parsing part ---
@@ -185,7 +200,7 @@ public class SynAn extends Phase {
 				add(node, Symbol.Term.TYP, EXPECTED_SYMBOLS_STR + "'typ'" + GOT_STR + currSymb.token.toString());
 				node.add(parseParDecl());
 				CheckAndSkip(Symbol.Term.SEMIC, SEMIC_ERR_STRING + currSymb.token.toString());
-				break;
+				break;EXPR_ADD_REST_ERR_STR
 			case VAR:
 				add(node, Symbol.Term.VAR, EXPECTED_SYMBOLS_STR + "'var'" + GOT_STR  + currSymb.token.toString());
 				node.add(parseParDecl());
@@ -264,9 +279,481 @@ public class SynAn extends Phase {
 		return node;
 	}
 
+	
+	private DerNode parseType() {
+		DerNode node = new DerNode(DerNode.Nont.Type);
+		switch (currSymb.token) {
+			case IDENTIFIER:
+				add(node, Symbol.Term.IDENTIFIER, IDENTIFIER_ERR_STRING + currSymb.token.toString());
+				break;
+
+			case LPARENTHESIS:
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+
+			case VOID:
+				add(node, Symbol.Term.VOID, EXPECTED_SYMBOLS_STR + "VOID" + GOT_STR + currSymb.token.toString());
+				break;
+
+			case BOOL:
+				add(node, Symbol.Term.BOOL, EXPECTED_SYMBOLS_STR + "BOOL" + GOT_STR + currSymb.token.toString());
+				break;
+
+			case CHAR:
+				add(node, Symbol.Term.CHAR, EXPECTED_SYMBOLS_STR + "CHAR" + GOT_STR + currSymb.token.toString());
+				break;
+
+			case INT:
+				add(node, Symbol.Term.INT, EXPECTED_SYMBOLS_STR + "INT" + GOT_STR + currSymb.token.toString());
+				break;
+			
+			case PTR:
+				add(node, Symbol.Term.PTR, EXPECTED_SYMBOLS_STR + "PTR" + GOT_STR + currSymb.token.toString());
+				node.add(parseType());
+				break;
+
+			case ARR:
+				add(node, Symbol.Term.ARR, EXPECTED_SYMBOLS_STR + "ARR" + GOT_STR + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LBRACKET, LBRACKET_ERR_STRING + currSymb.token.toString());
+				node.add(parseExpr());
+				CheckAndSkip(Symbol.Term.RBRACKET, RBRACKET_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				break;
+
+			case REC:
+				add(node, Symbol.Term.REC, EXPECTED_SYMBOLS_STR + "REC" + GOT_STR + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseCompDecls());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), TYPE_ERR_STR);
+
+		}
+
+		return node;
+	}
+
+	private DerNode parseBodyEps() {
+		DerNode node = new DerNode(DerNode.Nont.BodyEps);
+		switch (currSymb.token) {
+			case SEMIC:
+				break;
+
+			case ASSIGN:
+				add(node, Symbol.Term.ASSIGN, EXPECTED_SYMBOLS_STR + "=" + GOT_STR + currSymb.token.toString());
+				node.add(parseExpr());
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), BODY_ERR_STR);
+
+		}
+
+		return node;
+	}
+
+	private DerNode parseCompDecls() {
+		DerNode node = new DerNode(DerNode.Nont.CompDecls);
+		switch (currSymb.token) {
+			case IDENTIFIER:
+				node.add(parseParDecl());
+				node.add(parseCompDeclsRest());
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), RECORD_DECL_ERR_STR);
+
+		}
+
+		return node;
+	}
+
+	private DerNode parseCompDeclsRest() {
+		DerNode node = new DerNode(DerNode.Nont.CompDeclsRest);
+		switch (currSymb.token) {
+			case RPARENTHESIS:
+				break;
+
+			case COMMA:
+				CheckAndSkip(Symbol.Term.COMMA, COMMA_ERR_STRING + currSymb.token.toString());
+				node.add(parseParDecl());
+				node.add(parseCompDeclsRest());
+				break;
+
+			default:
+				throw new Report.Error(currSymb.location(), RECORD_DECL_ERR_STR);
+
+		}
+
+		return node;
+	}
 
 
+	private boolean IsOperator() {
+		switch (currSymb.token) {
+			case IDENTIFIER:
+			case LPARENTHESIS:
+			case ADD:
+			case SUB:
+			case NOT:
+			case ADDR:
+			case DATA:
+			case NEW:
+			case DEL:
+			case PTRCONST:
+			case BOOLCONST:
+			case VOIDCONST:
+			case CHARCONST:
+			case STRCONST:
+			case INTCONST:
+				return true;
+			default:
+				return false;
+		}
+	}
 
+	private DerNode parseExpr() {
+		DerNode node = new DerNode(DerNode.Nont.Expr);
+		if (IsOperator()) {
+			node.add(parseDisjExpr());
+			node.add(parseDisjExprRest());
+		} else {
+			switch (currSymb.token) {
+				case LBRACE:
+					CheckAndSkip(Symbol.Term.LBRACE, LBRACE_ERR_STRING + currSymb.token.toString());
+					node.add(parseStmts());
+					CheckAndSkip(Symbol.Term.COLON, COLON_ERR_STRING + currSymb.token.toString());
+					node.add(parseExpr());
+					node.add(parseWhereEps());
+					CheckAndSkip(Symbol.Term.RBRACE, RBRACE_ERR_STRING + currSymb.token.toString());
+				default:
+					throw new Report.Error(currSymb.location(), EXPR_ERR_STR);
+			}
+		}
+
+		return node;
+	}
+
+	private DerNode parseDisjExpr() {
+		DerNode node = new DerNode(DerNode.Nont.DisjExpr);
+		if (IsOperator()) {
+			node.add(parseConjExpr());
+			node.add(parseConjExprRest());
+		} else {
+			throw new Report.Error(currSymb.location(), EXPR_DISJ_ERR_STR);
+		}
+
+		return node;
+	}
+
+	private boolean IsEndOfExpresion(){
+		switch (currSymb.token) {
+			case SEMIC:
+			case RPARENTHESIS:
+			case COLON:
+			case COMMA:
+			case ASSIGN:
+			case RBRACKET:
+			case RBRACE:
+			case THEN:
+			case DO:
+			case WHERE:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	private DerNode parseDisjExprRest() {
+		DerNode node = new DerNode(DerNode.Nont.DisjExprRest);
+		if (!IsEndOfExpresion()) {
+			switch (currSymb.token) {
+				case IOR:
+					add(node, Symbol.Term.IOR, EXPECTED_SYMBOLS_STR + "|" + GOT_STR + currSymb.token.toString());
+					node.add(parseDisjExpr());
+					node.add(parseDisjExprRest());
+					break;
+				case XOR:
+					add(node, Symbol.Term.XOR, EXPECTED_SYMBOLS_STR + "^" + GOT_STR + currSymb.token.toString());
+					node.add(parseDisjExpr());
+					node.add(parseDisjExprRest());
+					break;
+
+				default:
+					throw new Report.Error(currSymb.location(), EXPR_DISJ_REST_ERR_STR);
+			}
+		}
+
+		return node;
+	}
+
+	private DerNode parseConjExpr() {
+		DerNode node = new DerNode(DerNode.Nont.ConjExpr);
+		if (IsOperator()) {
+			node.add(parseRelExpr());
+			node.add(parseRelExprRest());
+		} else {
+			throw new Report.Error(currSymb.location(), EXPR_CONJ_ERR_STR);
+		}
+
+		return node;
+	}
+
+	private boolean IsDisjExpresion(){
+		return IsEndOfExpresion() || currSymb.token == Symbol.Term.IOR || currSymb.token == Symbol.Term.XOR;
+	}
+
+	private DerNode parseConjExprRest() {
+		DerNode node = new DerNode(DerNode.Nont.ConjExprRest);
+		if (!IsDisjExpresion()) {
+			switch (currSymb.token) {
+				case AND:
+					add(node, Symbol.Term.AND, EXPECTED_SYMBOLS_STR + "&" + GOT_STR + currSymb.token.toString());
+					node.add(parseConjExpr());
+					node.add(parseConjExprRest());
+					break;
+
+				default:
+					throw new Report.Error(currSymb.location(), EXPR_CONJ_REST_ERR_STR);
+			}
+		}
+
+		return node;
+	}
+
+	private DerNode parseRelExpr() {
+		DerNode node = new DerNode(DerNode.Nont.RelExpr);
+		if (IsOperator()) {
+			node.add(parseAddExpr());
+			node.add(parseAddExprRest());
+		} else {
+			throw new Report.Error(currSymb.location(), EXPR_REL_ERR_STR);
+		}
+
+		return node;
+	}
+
+	private boolean IsConjExpresion(){
+		return IsDisjExpresion() || currSymb.token == Symbol.Term.AND;
+	}
+
+	private DerNode parseRelExprRest() {
+		DerNode node = new DerNode(DerNode.Nont.RelExprRest);
+		if (!IsConjExpresion()) {
+			switch (currSymb.token) {	
+				case EQU:
+					add(node, Symbol.Term.EQU, EXPECTED_SYMBOLS_STR + "==" + GOT_STR + currSymb.token.toString());
+					node.add(parseRelExpr());
+					node.add(parseRelExprRest());
+					break;
+
+				case NEQ:
+					add(node, Symbol.Term.NEQ, EXPECTED_SYMBOLS_STR + "!=" + GOT_STR + currSymb.token.toString());
+					node.add(parseRelExpr());
+					node.add(parseRelExprRest());
+					break;
+
+				case LEQ:
+					add(node, Symbol.Term.LEQ, EXPECTED_SYMBOLS_STR + "<=" + GOT_STR + currSymb.token.toString());
+					node.add(parseRelExpr());
+					node.add(parseRelExprRest());
+					break;
+				
+				case GEQ:
+					add(node, Symbol.Term.GEQ, EXPECTED_SYMBOLS_STR + ">=" + GOT_STR + currSymb.token.toString());
+					node.add(parseRelExpr());
+					node.add(parseRelExprRest());
+					break;
+
+				case LTH:
+					add(node, Symbol.Term.LTH, EXPECTED_SYMBOLS_STR + "<" + GOT_STR + currSymb.token.toString());
+					node.add(parseRelExpr());
+					node.add(parseRelExprRest());
+					break;
+
+				case GTH:
+					add(node, Symbol.Term.GTH, EXPECTED_SYMBOLS_STR + ">" + GOT_STR + currSymb.token.toString());
+					node.add(parseRelExpr());
+					node.add(parseRelExprRest());
+					break;
+
+				default:
+					throw new Report.Error(currSymb.location(), EXPR_REL_REST_ERR_STR);
+			}
+		}
+
+		return node;
+	}
+
+	private DerNode parseAddExpr() {
+		DerNode node = new DerNode(DerNode.Nont.AddExpr);
+		if (IsOperator()) {
+			node.add(parseMulExpr());
+			node.add(parseMulExprRest());
+		} else {
+			throw new Report.Error(currSymb.location(), EXPR_ADD_ERR_STR);
+		}
+
+		return node;
+	}
+
+	private boolean IsRelExpresion(){
+		if (IsConjExpresion()) {
+			return true;
+		}
+
+		switch (currSymb.token) {
+			case EQU:
+			case NEQ:
+			case LEQ:
+			case GEQ:
+			case LTH:
+			case GTH:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	private DerNode parseAddExprRest() {
+		DerNode node = new DerNode(DerNode.Nont.AddExprRest);
+		if (!IsRelExpresion()) {
+			switch (currSymb.token) {	
+				case ADD:
+					add(node, Symbol.Term.ADD, EXPECTED_SYMBOLS_STR + "+" + GOT_STR + currSymb.token.toString());
+					node.add(parseAddExpr());
+					node.add(parseAddExprRest());
+					break;
+
+				case SUB:
+					add(node, Symbol.Term.SUB, EXPECTED_SYMBOLS_STR + "-" + GOT_STR + currSymb.token.toString());
+					node.add(parseAddExpr());
+					node.add(parseAddExprRest());
+					break;
+
+				default:
+					throw new Report.Error(currSymb.location(), EXPR_ADD_REST_ERR_STR);
+			}
+		}
+
+		return node;
+	}
+
+	private DerNode parseMulExpr() {
+		DerNode node = new DerNode(DerNode.Nont.MulExpr);
+		if (IsOperator()) {
+			node.add(parsePrefExpr());
+		} else {
+			throw new Report.Error(currSymb.location(), EXPR_MUL_ERR_STR);
+		}
+
+		return node;
+	}
+
+	private boolean IsAddExpresion(){
+		return IsRelExpresion() || currSymb.token == Symbol.Term.ADD || currSymb.token == Symbol.Term.SUB;
+	}
+
+
+	private DerNode parseMulExprRest() {
+		DerNode node = new DerNode(DerNode.Nont.MulExprRest);
+		if (!IsAddExpresion()) {
+			switch (currSymb.token) {	
+				case MUL:
+					add(node, Symbol.Term.MUL, EXPECTED_SYMBOLS_STR + "*" + GOT_STR + currSymb.token.toString());
+					node.add(parseMulExpr());
+					node.add(parseMulExprRest());
+					break;
+
+				case DIV:
+					add(node, Symbol.Term.DIV, EXPECTED_SYMBOLS_STR + "/" + GOT_STR + currSymb.token.toString());
+					node.add(parseMulExpr());
+					node.add(parseMulExprRest());
+					break;
+
+				case MOD:
+					add(node, Symbol.Term.MOD, EXPECTED_SYMBOLS_STR + "%" + GOT_STR + currSymb.token.toString());
+					node.add(parseMulExpr());
+					node.add(parseMulExprRest());
+					break;
+
+				default:
+					throw new Report.Error(currSymb.location(), EXPR_MUL_REST_ERR_STR);
+			}
+		}
+
+		return node;
+	}
+
+
+	private DerNode parsePrefExpr() {
+		DerNode node = new DerNode(DerNode.Nont.PrefExpr);
+		switch (currSymb.token) {
+			case IDENTIFIER:
+				node.add(parsePstfExpr());
+				node.add(parsePstfExprRest());
+				break;
+			case LPARENTHESIS:
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseExpr());
+				node.add(parseCastEps());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+			case ADD:
+				add(node, Symbol.Term.ADD, EXPECTED_SYMBOLS_STR + "+" + GOT_STR + currSymb.token.toString());
+				node.add(parsePrefExpr());
+				break;
+			case SUB:
+				add(node, Symbol.Term.SUB, EXPECTED_SYMBOLS_STR + "-" + GOT_STR + currSymb.token.toString());
+				node.add(parsePrefExpr());
+				break;
+			case NOT:
+				add(node, Symbol.Term.NOT, EXPECTED_SYMBOLS_STR + "!" + GOT_STR + currSymb.token.toString());
+				node.add(parsePrefExpr());
+				break;
+			case ADDR:
+				add(node, Symbol.Term.ADDR, EXPECTED_SYMBOLS_STR + "$" + GOT_STR + currSymb.token.toString());
+				node.add(parsePrefExpr());
+				break;
+			case DATA:
+				add(node, Symbol.Term.DATA, EXPECTED_SYMBOLS_STR + "@" + GOT_STR + currSymb.token.toString());
+				node.add(parsePrefExpr());
+				break;
+			case NEW:
+				add(node, Symbol.Term.NEW, EXPECTED_SYMBOLS_STR + "NEW" + GOT_STR + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseType());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+			case DEL:
+				add(node, Symbol.Term.DEL, EXPECTED_SYMBOLS_STR + "DEL" + GOT_STR + currSymb.token.toString());
+				CheckAndSkip(Symbol.Term.LPARENTHESIS, LPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				node.add(parseExpr());
+				CheckAndSkip(Symbol.Term.RPARENTHESIS, RPARENTHESIS_ERR_STRING + currSymb.token.toString());
+				break;
+
+			case PTRCONST:
+			case BOOLCONST:
+			case VOIDCONST:
+			case CHARCONST:
+			case STRCONST:
+			case INTCONST:
+				node.add(parsePstfExpr());
+				node.add(parsePstfExprRest());
+				break;
+				
+			default:
+				throw new Report.Error(currSymb.location(), EXPR_PREF_ERR_STR);
+		}
+
+		return node;
+	}
+
+	// TODO Cast
 
 	/*
 
@@ -405,7 +892,7 @@ public class SynAn extends Phase {
 		switch (currSymb.token) {
 			case SEMIC:
 				break;
-			case ASSIGN:
+				case ASSIGN:
 				add(node, Symbol.Term.ASSIGN, EXPECTED_SYMBOLS_STR + "= got: " + currSymb.token.toString());
 				//System.out.println("before " + currSymb.token.toString());
 				node.add(parseExpr());
