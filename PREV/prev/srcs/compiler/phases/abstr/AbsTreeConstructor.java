79ce3e3bd71f59b494a31690d9a4f5c5326abf21
RokKos <rocki5555@gmail.com>
2019-03-23 13:56:13 +0100

    PREV - Abstr : Type finished

--- PREV/prev/srcs/compiler/phases/abstr/AbsTreeConstructor.java
+++ PREV/prev/srcs/compiler/phases/abstr/AbsTreeConstructor.java
@@ -7,6 +7,7 @@ import java.util.*;
 import compiler.common.report.*;
 import compiler.data.dertree.*;
 import compiler.data.dertree.visitor.*;
+import compiler.data.symbol.Symbol.Term;
 import compiler.data.abstree.*;
 
 /**
@@ -15,6 +16,11 @@ import compiler.data.abstree.*;
  * @author sliva
  */
 public class AbsTreeConstructor implements DerVisitor<AbsTree, AbsTree> {
+
+	private final String PTR_NODE = "Expected PTR node";
+	private final String ARR_NODE = "Expected ARR node";
+	private final String GOT_STR = " got: ";
+	private final String TOO_MANY_NODES = "There are zore or more than 3 nodes";
 
 	@Override
 	public AbsTree visit(DerLeaf leaf, AbsTree visArg) {
@@ -61,9 +67,15 @@ public class AbsTreeConstructor implements DerVisitor<AbsTree, AbsTree> {
 						return new AbsVarDecl(loc, parDecl.name, parDecl.type);
 					}
 					
+					case TYP: {
+						AbsParDecl parDecl = (AbsParDecl) node.subtree(1).accept(this, null);
+						Location loc = new Location(typeOfDecleration, parDecl);
+						return new AbsTypDecl(loc, parDecl.name, parDecl.type);
+					}
 
 					
 					default:
+						// TODO: Error
 						break;
 				}
 			}
@@ -78,33 +90,63 @@ public class AbsTreeConstructor implements DerVisitor<AbsTree, AbsTree> {
 
 			case Type: {
 				if (node.numSubtrees() == 1) {
-					DerLeaf primitiveTypeNode = (DerLeaf) node.subtree(0);
-					AbsAtomType.Type primitiveType;
-					switch (primitiveTypeNode.symb.token) {
-						case VOID:
-							primitiveType = AbsAtomType.Type.VOID;
-							break;
 
-						case BOOL:
-							primitiveType = AbsAtomType.Type.BOOL;
-							break;
+					// This is only check for ( Type )
+					if (node.subtree(0) instanceof DerNode) {
+						return node.subtree(0).accept(this,null);
+					} else if (node.subtree(0) instanceof DerLeaf) {
+						DerLeaf primitiveTypeNode = (DerLeaf) node.subtree(0);
+						AbsAtomType.Type primitiveType;
+						switch (primitiveTypeNode.symb.token) {
+							case VOID:
+								primitiveType = AbsAtomType.Type.VOID;
+								break;
+
+							case BOOL:
+								primitiveType = AbsAtomType.Type.BOOL;
+								break;
+							
+							case CHAR:
+								primitiveType = AbsAtomType.Type.CHAR;
+								break;
+
+							case INT:
+								primitiveType = AbsAtomType.Type.INT;
+								break;
 						
-						case CHAR:
-							primitiveType = AbsAtomType.Type.CHAR;
-							break;
+							default:
+								// TODO: Error (Maybe)
+								return new AbsTypName(primitiveTypeNode.location(), primitiveTypeNode.symb.lexeme);
+						}
 
-						case INT:
-							primitiveType = AbsAtomType.Type.INT;
-							break;
-					
-						default:
-							return new AbsTypName(primitiveTypeNode.location(), primitiveTypeNode.symb.lexeme);
+						return new AbsAtomType(primitiveTypeNode.location(), primitiveType);
+					}
+				} else if (node.numSubtrees() == 2) {
+					DerLeaf ptr = (DerLeaf)node.subtree(0);
+					if (ptr.symb.token != Term.PTR) {
+						throw new Report.Error(ptr.location(), PTR_NODE + GOT_STR + ptr.symb.token.toString());
 					}
 
-					return new AbsAtomType(primitiveTypeNode.location(), primitiveType);
+					AbsType subType = (AbsType) node.subtree(1).accept(this, null);
+					Location loc = new Location(ptr, subType);
+					return new AbsPtrType(loc, subType);
+
+				} else if (node.numSubtrees() == 3) {
+					DerLeaf arr = (DerLeaf)node.subtree(0);
+					if (arr.symb.token != Term.ARR) {
+						throw new Report.Error(arr.location(), ARR_NODE + GOT_STR + arr.symb.token.toString());
+					}
+
+					AbsExpr length = (AbsExpr) node.subtree(1).accept(this, null);
+					AbsType elemType = (AbsType) node.subtree(2).accept(this, null);
+					Location loc = new Location (arr, elemType);
+					return new AbsArrType(loc, length, elemType);
+
 				} else {
-					// TODO: multiple types
+					// TODO: Error
+					throw new Report.Error(node.location(), TOO_MANY_NODES + GOT_STR + node.numSubtrees());
 				}
+
 			}
 
 
