cb78035a669b318850d6a154a89e7eb4670825dc
RokKos <rocki5555@gmail.com>
2019-12-20 09:36:13 +0100

    OUI: DN03 - Working bot (but doesn't evade yet)

--- OUI/planet-lia/rokkos/my_bot.py
+++ OUI/planet-lia/rokkos/my_bot.py
@@ -25,31 +25,30 @@ class MyBot(Bot):
         self.half_map_Width = int(self.mapWidth / 2)
         self.map = initial_data["map"]
         self.left_bot = False
+        self.reset_player = False
         self.possible_moves = [Direction.LEFT, Direction.RIGHT, Direction.UP, Direction.DOWN]
         
-        self.my_distance_x = int(initial_data["yourUnit"]["x"])
-        self.my_map_x = self.my_distance_x
+        self.my_x = int(initial_data["yourUnit"]["x"])
         self.my_y = int(initial_data["yourUnit"]["y"])
-        self.my_part_of_map = [[False for i in range(0, self.half_map_Width)] for j in range(0, self.mapHeight)]
-        if (self.my_distance_x == 0):
+        #self.my_part_of_map = [[False for i in range(0, self.half_map_Width)] for j in range(0, self.mapHeight)]
+        if (self.my_x == 0):
             self.left_bot = True
-            for y in range(0,self.mapHeight):
-                for x in range(0, self.half_map_Width):
-                    self.my_part_of_map[y][x] = self.map[y][x]
+        #    for y in range(0,self.mapHeight):
+        #        for x in range(0, self.half_map_Width):
+        #            self.my_part_of_map[y][x] = self.map[y][x]
         
-        elif(self.my_distance_x == 19):
-            self.my_map_x -= self.half_map_Width
+        elif(self.my_x == 19):
             self.left_bot = False
-            for y in range(0,self.mapHeight):
-                for x in range(self.half_map_Width, self.mapWidth):
-                    self.my_part_of_map[y][x - self.half_map_Width] = self.map[y][x - self.half_map_Width]
+        #    for y in range(0,self.mapHeight):
+        #        for x in range(self.half_map_Width, self.mapWidth):
+        #            self.my_part_of_map[y][x - self.half_map_Width] = self.map[y][x - self.half_map_Width]
         
         self.print_debug("::setup")
         # Print out the map
         self.print_part_of_map(self.map, self.mapHeight, self.mapWidth)
-        self.print_part_of_map(self.my_part_of_map, self.mapHeight, self.half_map_Width)
+        #self.print_part_of_map(self.my_part_of_map, self.mapHeight, self.half_map_Width)
         
-        self.Reset()
+        self.Reset(self.initial_data["coins"])
         
 
     def print_debug(self, text):
@@ -65,26 +64,28 @@ class MyBot(Bot):
                 map_line += "_" if mapy[y][x] else "#"
             self.print_debug(map_line)
 
-    def Reset(self):
+    def Reset(self, list_of_coins):
         self.print_debug("::Reset")
-        self.closest_coin = self.ChooseClosestCoin(self.initial_data["coins"])
+        self.closest_coin = self.ChooseClosestCoin(list_of_coins)
         self.print_debug("Closest coin: " + str(self.closest_coin))
 
         self.candidates = []
-        self.costs_for_field = [[inf for _ in range(self.half_map_Width)] for __ in range(self.mapHeight)]
-        self.print_debug("My coordinate: " + str(self.my_distance_x) + ", " + str(self.my_y))
-        self.costs_for_field[self.my_y][self.my_map_x] = self.ManhatanDistance(self.my_distance_x, self.my_y, self.closest_coin[0], self.closest_coin[1])    
+        self.costs_for_field = [[inf for _ in range(self.mapWidth)] for __ in range(self.mapHeight)]
+        self.print_debug("My coordinate: " + str(self.my_x) + ", " + str(self.my_y))
+        self.costs_for_field[self.my_y][self.my_x] = self.ManhatanDistance(self.my_x, self.my_y, self.closest_coin[0], self.closest_coin[1])    
         
 
-        #self.PushCandidate((self.costs_for_field[self.my_y][self.my_distance_x], (self.my_distance_x, self.my_y)))
-        self.FindAllCandidates()
+        #self.PushCandidate((self.costs_for_field[self.my_y][self.my_x], (self.my_x, self.my_y)))
+        #self.FindAllCandidates()
 
     def PushCandidate(self, elem):
         self.print_debug("::PushCandidate")
         heapq.heappush(self.candidates, elem)  
     def GetCandidate(self):
         self.print_debug("::GetCandidate")
-        return heapq.heappop(self.candidates)  
+        candidate = heapq.heappop(self.candidates)
+        self.print_debug(candidate)
+        return candidate
 
     def ChooseClosestCoin(self, list_of_coins):
         self.print_debug("::ChooseClosestCoin")
@@ -102,12 +103,21 @@ class MyBot(Bot):
             if(self.left_bot):
                 while True:
                     coordinate = (random.randint(0, self.half_map_Width), random.randint(0, self.mapHeight))
-                    if (self.my_part_of_map[coordinate[1], coordinate[0]]):
+
+                    if not (coordinate[0] >= 0 and coordinate[1] >= 0 and coordinate[0] < self.mapWidth and coordinate[1] < self.mapHeight):
+                        continue
+
+                    if (self.map[coordinate[1]][coordinate[0]]):
                         return coordinate
             else:
                 while True:
-                    coordinate = (random.randint(self.half_map_Width, self.mapWidth) - self.half_map_Width, random.randint(0, self.mapHeight))
-                    if (self.my_part_of_map[coordinate[1], coordinate[0]]):
+                    coordinate = (random.randint(self.half_map_Width, self.mapWidth), random.randint(0, self.mapHeight))
+                    
+                    if not (coordinate[0] >= 0 and coordinate[1] >= 0 and coordinate[0] < self.mapWidth and coordinate[1] < self.mapHeight):
+                        continue
+
+
+                    if (self.map[coordinate[1]][coordinate[0]]):
                         return coordinate
                 #return (-1, -1)
         elif (len(possible_coins) == 1):
@@ -115,11 +125,11 @@ class MyBot(Bot):
         else:
             coin1_x = possible_coins[0]["x"]
             coin1_y = possible_coins[0]["y"]
-            coin1_len = self.ManhatanDistance(self.my_distance_x,self.my_y, coin1_x, coin1_y)
+            coin1_len = self.ManhatanDistance(self.my_x,self.my_y, coin1_x, coin1_y)
 
             coin2_x = possible_coins[1]["x"]
             coin2_y = possible_coins[1]["y"]
-            coin2_len = self.ManhatanDistance(self.my_distance_x,self.my_y, coin2_x, coin2_y)
+            coin2_len = self.ManhatanDistance(self.my_x,self.my_y, coin2_x, coin2_y)
 
             if (coin1_len < coin2_len):
                 return (coin1_x, coin1_y)
@@ -131,6 +141,8 @@ class MyBot(Bot):
 
     def FindAllCandidates(self):
         self.print_debug("::FindAllCandidates")
+
+        
         for direction in self.possible_moves:
             d_dir = (0,0)
             if direction == Direction.LEFT:
@@ -143,26 +155,20 @@ class MyBot(Bot):
                 d_dir = (0,-1)
             
             new_y = self.my_y + d_dir[1]
-            new_x = self.my_distance_x + d_dir[0]
-            new_map_x = new_x
+            new_x = self.my_x + d_dir[0]
 
             if not (new_x >= 0 and new_y >= 0 and new_x < self.mapWidth and new_y < self.mapHeight):
                 continue
-
-            if (not self.left_bot):
-                new_map_x -= self.half_map_Width
-                self.print_debug("New map x:" + str(new_map_x))
             
             # TODO: Check also for saws
-            if (not self.my_part_of_map[new_y][new_map_x]):
+            if (not self.map[new_y][new_x]):
                 continue
 
             self.print_debug("New x:" + str(new_x) + " New y:" + str(new_y))
-            self.print_debug("New map x:" + str(new_map_x))
             curr_distance = self.ManhatanDistance(new_x, new_y, self.closest_coin[0], self.closest_coin[1])
-            if (curr_distance < self.costs_for_field[new_y][new_map_x]):
-                self.costs_for_field[new_y][new_map_x] = curr_distance
-                self.PushCandidate((self.costs_for_field[new_y][new_map_x] + 1, direction))
+            if (curr_distance < self.costs_for_field[new_y][new_x]):
+                self.costs_for_field[new_y][new_x] = curr_distance
+                self.PushCandidate((self.costs_for_field[new_y][new_x], direction))
 
     # Called repeatedly while the match is generating. Each
     # time you receive the current match state and can use
@@ -173,27 +179,24 @@ class MyBot(Bot):
         # TODO: Remove this code and implement a proper path finding!
 
 
-        self.my_distance_x = int(state["yourUnit"]["x"])
-        self.my_map_x = self.my_distance_x
+        self.my_x = int(state["yourUnit"]["x"])
         self.my_y = int(state["yourUnit"]["y"])
 
-        if (not self.left_bot):
-            self.my_map_x -= self.half_map_Width
-
-
-        if (self.my_distance_x == self.closest_coin[0] and  self.my_y == self.closest_coin[1]):
-            self.Reset()
-            self.print_debug("Dobil kovanec!!!")
+        if (self.reset_player):
+            self.reset_player = False
+            self.Reset(state["coins"])
             # TODO: Dodaj se kaj kar bos rabu
+
+        if (self.my_x == self.closest_coin[0] and  self.my_y == self.closest_coin[1]):
+            self.print_debug("Dobil kovanec!!!")
+            self.reset_player = True
+            
+        self.FindAllCandidates()
 
         closest_candidate = self.GetCandidate()
         candidate_cost = closest_candidate[0]
         candidate_move = closest_candidate[1]
         response.move_unit(candidate_move)
-
-        
-        
-        self.FindAllCandidates()
 
 # Connects your bot to match generator, don't change it.
 if __name__ == "__main__":
